1f5e3e1d642976871e52f29aaff823553ae0c852
ITBANK <ITBANK@DESKTOP-IIC90A6>
2017-09-08 21:00:23 +0900

    Revert "메세지"
    This reverts commit 318b72c39abd1147c65e1ca9de7f7b1c7b5b60bb.

--- airbnb/src/main/java/spring/controller/MypageController.java
+++ airbnb/src/main/java/spring/controller/MypageController.java
@@ -1,28 +1,14 @@
 package spring.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import spring.model.MessageDao;
 
 @Controller
 @RequestMapping("/mypage")
 public class MypageController {
-	@Autowired
-	private MessageDao messageDao;
-	
 
 	@RequestMapping("/rooms")
 	public String rooms() {
 		return "mypage/rooms";
-	}
-	
-	@RequestMapping("/message")
-	public String message(Model m, int member_no) {
-		m.addAttribute("count", messageDao.count(member_no));
-		m.addAttribute("message", messageDao.getMessage(member_no));
-		return "sub/message";
 	}
 }
