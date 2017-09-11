ea6f3772aef1ad8fe6af0b28058b2b93a584bbf9
최완우 <ccuuyyjj@gmail.com>
2017-09-11 17:43:52 +0900

    Spring Security 적용, Dao 수정

---
+++ airbnb/src/main/java/spring/settings/SecuritySettings.java
@@ -0,0 +1,35 @@
+package spring.settings;
+
+import org.apache.commons.dbcp2.BasicDataSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+@Configuration
+public class SecuritySettings extends WebSecurityConfigurerAdapter {
+	@Autowired
+	private BasicDataSource dataSource;
+	
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth
+			.inMemoryAuthentication()
+				.withUser("admin")
+					.password("admin")
+					.roles("ADMIN");
+		auth
+			.jdbcAuthentication()
+				.dataSource(dataSource)
+				.usersByUsernameQuery("select username, password, enabled from member where username = ?")
+				.authoritiesByUsernameQuery("select username, authority from member where username = ?");
+	}
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http
+			.formLogin();
+	}
+}
