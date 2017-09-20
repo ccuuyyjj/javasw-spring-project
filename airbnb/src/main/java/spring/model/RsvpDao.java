c064fe132693ed2ce91a0cd468823a6c66e5d32a
오지현 <IT202-06@DESKTOP-TCGKCP9>
2017-09-20 16:30:53 +0900

    예약 가능 요청

--- airbnb/src/main/java/spring/model/RsvpDao.java
+++ airbnb/src/main/java/spring/model/RsvpDao.java
@@ -1,5 +1,7 @@
 package spring.model;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
@@ -7,6 +9,8 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public class RsvpDao {
+	Logger log = LoggerFactory.getLogger(getClass());
+	
 	@Autowired
 	private JdbcTemplate jdbcTemplate;
 
@@ -16,11 +20,11 @@ public class RsvpDao {
 
 	public void insert(Rsvp rsvp) {
 		String sql = "insert into reservation values(reservation_seq.nextval, ? ,?, ?, "
-				+ "to_date(?, 'mm/dd/yyyy'), to_date(?, 'mm/dd/yyyy'), "
-				+ "?, ?, ?, sysdate,   ?, ?)";
+				+ "to_date(?, 'YYYY-MM-DD HH24:MI:SS'), to_date(?, 'YYYY-MM-DD HH24:MI:SS'), "
+				+ "?, ?, sysdate,  ?, ?, ?)";
 		Object[] args = new Object[] { 
 				rsvp.getRoom_no(), rsvp.getQuantity(), rsvp.getPhone(), 
-				rsvp.getStartdate(), rsvp.getEnddate(), 
+				rsvp.getStartdate(), rsvp.getEnddate(),  
 				rsvp.getTotalprice(), rsvp.getEtc(), rsvp.getProgress(), rsvp.getGuest_id(), rsvp.getR_id() };
 
 		jdbcTemplate.update(sql, args);
