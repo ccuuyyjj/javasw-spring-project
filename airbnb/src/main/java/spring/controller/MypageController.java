28ca8a7f9031a68deba7855112747a09f37fb80c
ITBANK <ITBANK@DESKTOP-IIC90A6>
2017-09-21 21:55:14 +0900

    .

--- airbnb/src/main/java/spring/controller/MypageController.java
+++ airbnb/src/main/java/spring/controller/MypageController.java
@@ -24,6 +24,7 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import spring.model.Member;
