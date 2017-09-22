c58e7258f82d8e4f45213d50ae755eec11c541e8
오지현 <IT202-06@DESKTOP-TCGKCP9>
2017-09-22 17:43:42 +0900

    관리자 메뉴 ui

--- airbnb/src/main/java/spring/controller/AdminController.java
+++ airbnb/src/main/java/spring/controller/AdminController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class AdminController {
 	@RequestMapping(value= {"/home", "/", ""})
 	public String home() {
-		return "admin/home";
+		return "admin/member/home";
 	}
 	@Controller
 	@RequestMapping(value= {"/admin/member"})
