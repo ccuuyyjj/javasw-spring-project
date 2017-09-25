16c483864c9c6a49a99d3cd7e953f9b682bb5f30
최완우 <ccuuyyjj@gmail.com>
2017-09-25 17:23:32 +0900

    관리자 회원관리 페이지

---
+++ airbnb/src/main/webapp/resource/js/admin_member.js
@@ -0,0 +1,63 @@
+function toggleEdit(target) {
+	console.log(target.tagName);
+	console.log(target.className);
+	if (target.tagName == "BUTTON" || target.tagName == "A") {
+		// var value = $(target).val();
+		// $(target).parent().text(value);
+	} else if ($(target).find("input[type=text]").length != 0) {
+		var value = $(target).find("input[type=text]").val();
+		$(target).text(value);
+	} else if (target.className == "m_name" || target.className == "m_phone"
+			|| target.className == "m_authority") {
+		var edit = $("<input>");
+		edit.attr("type", "text");
+		edit.attr("id", target.className.substring(2));
+		edit.css("width", (parseInt($(target).css("width")) - 58));
+		edit.val($(target).text());
+		$(target).empty();
+		$(target).append(edit);
+		var submit = $("<button>");
+		submit.attr("onclick", "modify(this);");
+		submit.text("저장");
+		$(target).append(submit);
+	} else if(target.className == "m_pwreset") {
+		var no = $(target).parent().find(".m_no").text();
+		var url = "/airbnb/admin/member/tempPw?no=" + no;
+		location.href(url);
+	}
+}
+function modify(target) {
+	var no = $(target).parent().parent().find(".m_no").text();
+	var type = $(target).parent().find("input[type=text]").attr("id");
+	var value = $(target).parent().find("input[type=text]").val();
+	var data = "no=" + no + "&" + type + "=" + value;
+	$.ajax({
+		url : "/airbnb/admin/member/modify",
+		type : "post",
+		data : data,
+		success : function(res) {
+			if (res == "true")
+				$(target).parent().text(value);
+			else
+				$(target).parent().find("input[type=text]").css("border-color",
+						"red");
+		}
+	});
+}
+function tempPw(no){
+	var data = "no=" + no;
+	$.ajax({
+		url : "/airbnb/admin/member/tempPw",
+		type : "post",
+		data : data,
+		success : function(res) {
+			$(".tempPw").html("<h3>새 비밀번호 : " + res + "</h3>");
+			$(".gen").remove();
+		}
+	});
+}
+$(document).ready(function() {
+	$(".m_row").on("click", function(e) {
+		toggleEdit(e.target);
+	});
+});
